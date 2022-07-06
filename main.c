1d6b65f184314dab3d10191576af1c094e1e627e
Matthew Herman <mattherman11@gmail.com>
2022-07-06 12:36:47 -0500

    Added stack and implemented constant and negate instructions

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "common.h"
 #include "chunk.h"
-#include "debug.h"
 #include "vm.h"
 
 int main(int argc, const char* argv[]) {
@@ -13,9 +12,10 @@ int main(int argc, const char* argv[]) {
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
