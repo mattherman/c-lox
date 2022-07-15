dc76e529f03de8468b418f113b1c3a7f4603cc0c
Matthew Herman <mattherman11@gmail.com>
2022-07-14 19:35:47 -0500

    Started parser

--- src/compiler.c
+++ src/compiler.c
@@ -4,9 +4,40 @@
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct {
+    Token current;
+    Token previous;
+} Parser;
+
+Parser parser;
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void expression() {
+
+}
+
+static void consume(TokenType tokenType, char* errorMessage) {
+
+}
+
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return false;
 }
