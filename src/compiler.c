790c078d9cec787d8be278cc56243622ae0f4fab
Matthew Herman <mattherman11@gmail.com>
2023-02-26 22:36:53 -0600

    Small cleanup

--- src/compiler.c
+++ src/compiler.c
@@ -18,15 +18,15 @@ typedef struct {
 
 typedef enum {
     PREC_NONE,
-    PREC_ASSIGNMENT,
-    PREC_OR,
-    PREC_AND,
-    PREC_EQUALITY,
-    PREC_COMPARISON,
-    PREC_TERM,
-    PREC_FACTOR,
-    PREC_UNARY,
-    PREC_CALL,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM,       // + -
+    PREC_FACTOR,     // * /
+    PREC_UNARY,      // ! -
+    PREC_CALL,       // . ()
     PREC_PRIMARY
 } Precedence;
 
@@ -46,6 +46,8 @@ static Chunk* currentChunk() {
 }
 
 static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
     fprintf(stderr, "[line %d] Error", token->line);
 
     if (token->type == TOKEN_EOF) {
